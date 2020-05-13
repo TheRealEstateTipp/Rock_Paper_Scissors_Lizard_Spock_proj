0534838b20d709a0fbc2bbc4f837e4e2e3251303
unknown <mstiff1983@gmail.com>
2020-05-13 15:38:03 -0400

    I have added the name and List variables and the method to enter your player name and a method for gesture to the Player Abstract Class.

--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,23 @@ namespace RPSLS
 {
     public abstract class Player
     {
+        //Member Variables
+        public string PlayerName;
+        public List<string> Gesture = new List<string>{ ("Rock"), ("Paper"), ("Scissor"), ("Lizard"), ("Spock") };
+
+
+        
+       
+
+        public virtual void playername()
+        {
+            Console.WriteLine("Please Enter Player's Name");
+            string userInput = Console.ReadLine();
+        }
+
+        public abstract void Gestures();
+
+
+
     }
 }
