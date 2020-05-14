de9deb49e55afa05bd1dd86f1171a6b8da5dca1d
unknown <mstiff1983@gmail.com>
2020-05-14 15:20:43 -0400

    I have added the to-do or the tasks that I want to complete on my Stimulator class.

--- RPSLS/Simulator.cs
+++ RPSLS/Simulator.cs
@@ -8,5 +8,18 @@ namespace RPSLS
 {
     class Simulator
     {
+        public Player playerOne;
+        public Player playerTwo;
+
+
+        //1. What are the rules?
+        //2. Single Player or Multi-Player? (Human vs. Human or Human vs. Computer)
+        //3. What are players name?
+        //4. Player Choose Gestures
+        //5. Compare the gestures
+        //6. Who wins the round? Is this the last round?
+        //7. Is there a game winner? (If yes, who is the winner and would you like to play again? If no, loop back to Step 4.
+        
+        
     }
 }
